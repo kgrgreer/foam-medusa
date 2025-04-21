7be7eed69aa4144df77237f5d23f07bc6b52685c
Joel Hughes <joel@nanopay.net>
2025-04-21 12:28:37 -0400

    build - update build scripts and pom to match foam3 build changes

--- deployment/mm/pom.js
+++ deployment/mm/pom.js
@@ -1,13 +1,11 @@
 foam.POM({
   name:'mm',
-  tasks: [ // copy to application mm pom
-    function setRunArgs() {
-      // configure instance as mediator
-      RUN_ARGS += ` -m`;
-      this.SUPER();
-    }
-  ],
   projects: [
     { name: '../m/pom' }
+  ],
+  tasks: [
+    function setRunArgs() {
+      RUN_ARGS += ' -m';
+    }
   ]
 });
